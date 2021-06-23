dadd96edf870aa2bc41022a7a7234c209c834dcc
rorode8 <rorode8@hotmail.com>
2021-06-23 09:49:15 -0500

    Add files via upload

--- backwardFiniteDifference.m
+++ backwardFiniteDifference.m
@@ -1,9 +1,9 @@
 function ffd = backwardFiniteDifference(f,x,h,m,n)
 %reject if m>2 or n>2
-coefficients = [NaN NaN -1 1 ;
-                NaN 1/2 -2 -3/2 ]
+coefficients = [NaN NaN 1 -1 ;
+                NaN 3/2 -2 1/2];
 coefficients(:,:,2) = [ NaN 1 -2 1 ;
-                        2 -5 4 -1]
+                        2 -5 4 -1];
 
 c = coefficients(n,:,m);
 c = c(not(isnan(c)));
@@ -11,7 +11,8 @@ c = c(not(isnan(c)));
 ffd = 0;
 i=0;
     for cof =c
-        ffd = ffd+ cof*(x-i*h);
+        ffd = ffd+ cof*f(x-i*h);
         i=i+1;
     end
+    ffd=ffd/(h^m);
 end
--- forwardFiniteDifference.m
+++ forwardFiniteDifference.m
@@ -1,18 +1,19 @@
 function ffd = forwardFiniteDifference(f,x,h,m,n)
 %reject if m>2 or n>2
 coefficients = [-1 1 NaN NaN;
-                -3/2 2 -1/2 NaN]
+                -3/2 2 -1/2 NaN];
 coefficients(:,:,2) = [1 -2 1 NaN;
-                        2 -5 4 -1]
+                        2 -5 4 -1];
 
-c = coefficients(n,:,m)
-c = c(not(isnan(c)))
+c = coefficients(n,:,m);
+c = c(not(isnan(c)));
 
 ffd = 0;
 i=0;
     for cof =c
-        ffd = ffd+ cof*(x+i*h);
+        ffd = ffd+ cof*f(x+i*h);
         i=i+1;
     end
+    ffd=ffd/(h^m);
 end
 
